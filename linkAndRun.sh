292562dc9c51e70340ece3bc5ddc43a6f27cf5fd
Bjorn Hamre <bha@ambita.com>
2018-09-06 14:00:26 +0200

    Add maven plugin to build image

--- linkAndRun.sh
+++
@@ -1,8 +0,0 @@
-mvn clean install
-rm -rf libs
-mkdir libs
-rm -rf image
-cp api/target/jlink-api-1.0-SNAPSHOT.jar libs/
-cp app/target/jlink-app-1.0-SNAPSHOT.jar libs/
-jlink --module-path "/c/java/jdk-10/jmods/;libs" --add-modules no.hamre.java9workshop.jlink.api,no.hamre.java9workshop.jlink.app --output image/
-./image/bin/java -m no.hamre.java9workshop.jlink.app/no.hamre.java9workshop.jlink.app.Main
